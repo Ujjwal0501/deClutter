19d4923c4a81b3efe4f515f82f8f4c3d366b19f7
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2019-06-02 02:58:58 +0530

    ui controls functional

--- myscript.js
+++ myscript.js
@@ -8,14 +8,14 @@ function inform() {
 
 function init () {
 	declutter_list1 = document.getElementsByTagName('body')[0].getElementsByTagName('*');
-	document.addEventListener("keyup", function(event) {
-		var code = event.which || event.keyCode;
-		if (code == 17) declutter_flag = 0;
-	});
-	document.addEventListener("keydown", function(event) {
-		var code = event.which || event.keyCode;
-		if (code == 17) declutter_flag = 1;
-	});
+	// document.addEventListener("keyup", function(event) {
+	// 	var code = event.which || event.keyCode;
+	// 	if (code == 17) declutter_flag = 0;
+	// });
+	// document.addEventListener("keydown", function(event) {
+	// 	var code = event.which || event.keyCode;
+	// 	if (code == 17) declutter_flag = 1;
+	// });
 	for (var i = 0; i < declutter_list1.length; i++) {
 		declutter_list1[i].addEventListener("click", function (event) {
 			if (declutter_flag && this == event.target) this.style.display = "none";
@@ -61,28 +61,45 @@ function create_layout () {
 	
 	// set properties of container division
 	div.style.height = "20px";
-	div.style.width = "80px";
 	div.style.position = "fixed";
 	div.style.top = "50px";
-	div.style.right = "0px";
+	div.style.right = "8px";
 	div.style.zIndex = "9999999999999999";
-	div.style.backgroundColor = "#03ffffff";
+	div.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
 	
 	// shifting the control layout to left or right
 	rightalign.addEventListener("click", () => {
 		div.style.left = "auto";
-		div.style.right = "0px";
+		div.style.right = "8px";
 		rightalign.style.display = "none";
-		leftalign.style.display = "block";
+		leftalign.style.display = "inline";
 	});
 	leftalign.addEventListener("click", () => {
 		div.style.right = "auto";
-		div.style.left = "0px";
+		div.style.left = "8px";
 		leftalign.style.display = "none";
-		rightalign.style.display = "block";
+		rightalign.style.display = "inline";
 	});
 	rightalign.style.display = "none";
 	
+	// play pause ui switch
+	start.addEventListener("click", () => {
+		declutter_flag = 1;
+		close.style.display = "none";
+		start.style.display = "none";
+		pause.style.display = "inline";
+	});
+	pause.addEventListener("click", () => {
+		declutter_flag = 0;
+		close.style.display = "inline";
+		pause.style.display = "none";
+		start.style.display = "inline";
+	});
+	pause.style.display = "none";
+	
+	close.addEventListener("click", () => {
+		div.style.display = "none";
+	})
 	
 	document.body.prepend(div);
 }
